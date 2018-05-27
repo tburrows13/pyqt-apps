520d5a094000b3a798d42b71848049d56c3195be
Tom Burrows <tomburrows13@btinternet.com>
2018-05-27 20:30:14 +0100

    Fully functional calculator

--- calculator.py
+++ calculator.py
@@ -9,26 +9,33 @@ Calculator
 """
 
 import sys
+import re
 from PyQt5.QtWidgets import QLineEdit, QPushButton, QApplication, QDialog, \
-	QLabel, QTextEdit, QVBoxLayout, QHBoxLayout, QWidget, QGridLayout, QFrame
+	QLabel, QTextEdit, QVBoxLayout, QHBoxLayout, QWidget, QGridLayout, QFrame, \
+	QGroupBox
 from PyQt5.QtGui import QFont
+from PyQt5.Qt import Qt
 
-
+LINES_TO_SHOW = 4
 
 
 class Window(QWidget):
 	def __init__(self, parent=None):
 		super(Window, self).__init__(parent)
 
-		self.display = QLabel(self)
+		self.setWindowTitle("Calculator")
 
-		display_font = QFont("Menlo", 16)
-		display_font.setLetterSpacing(QFont.AbsoluteSpacing, 2)
-		self.display.setFont(display_font)
-		#self.display.setIndent(10)
-		self.display.setLineWidth(2)
-		self.display.setFrameStyle(QFrame.Panel | QFrame.Raised)
-		#self.display.setLineWidth(2)
+		self.display = QGroupBox(self)
+
+		self.inside_display = QVBoxLayout()
+
+		self.display_font = QFont("Menlo", 16)
+		self.display_font.setLetterSpacing(QFont.AbsoluteSpacing, 2)
+
+		self.lines = []
+		for i in range(2 * LINES_TO_SHOW + 1):
+			self.add_line()
+		self.display.setLayout(self.inside_display)
 
 		self.grid = "7 8 9r" \
 					"4 5 6r" \
@@ -47,12 +54,21 @@ class Window(QWidget):
 		self.add_button("/", 1, 4)
 		self.add_button("+", 2, 3)
 		self.add_button("-", 2, 4)
+		self.add_button("0", 3, 0)
+		self.add_button("(", 3, 1)
+		self.add_button(")", 3, 2)
+		self.add_button(".", 3, 3)
+		self.add_button("=", 3, 4)
 
 		self.layout = QVBoxLayout()
 		self.layout.addWidget(self.display)
 		self.layout.addLayout(self.grid_layout)
 
 		self.setLayout(self.layout)
+
+		self.previous_result = None
+		self.previous_expressions = []
+		self.current_expression = 0
 
 	def add_button(self, button_str, x, y, clicked_str=None):
 		if clicked_str is None:
@@ -61,19 +77,114 @@ class Window(QWidget):
 		self.grid_layout.addWidget(button, x, y)
 		button.clicked.connect(lambda: self.button_pressed(clicked_str))
 
+	def add_line(self, equals=None):
+		line = QLabel()
+		line.setFont(self.display_font)
+		line.setLineWidth(2)
+
+		if equals is not None:
+			line.setAlignment(Qt.AlignRight)
+			line.setText(equals)
+
+		self.lines.append(line)
+		self.inside_display.addWidget(line)
+
 	def button_pressed(self, key_str):
+		if key_str in ("x", "/", "+", "-") and len(self.lines[-1].text()) == 0:			# We need check if this is the first key pressed.
+			# If it is, add ANS to the beginning
+			self.button_pressed("ANS")
 		if key_str == "DEL":
-			self.display.setText(self.display.text()[:-1])
+			self.delete_pressed()
 		elif key_str == "AC":
-			self.display.setText("")
+			self.clear_pressed()
+		elif key_str == "=":
+			self.equals_pressed()
+		elif key_str == "ANS" and self.previous_result is None:
+			return
 		else:
-			self.display.setText(self.display.text() + key_str)
+			self.lines[-1].setText(self.lines[-1].text() + key_str)
 
-	def subtract(self):
-		print("Sub")
+	def delete_pressed(self):
+		current_line = self.lines[-1].text()
+		if current_line.endswith("ANS"):
+			to_delete = 3
+			# Deletes all 3 characters of ANS
+		else:
+			to_delete = 1
+		self.lines[-1].setText(current_line[:-to_delete])
 
-	def pressed(self):
-		print("Button pressed")
+	def clear_pressed(self):
+		self.lines[-1].setText("")
+
+	def equals_pressed(self):
+		to_eval = self.lines[-1].text()
+		self.previous_expressions.append(to_eval)
+		to_eval = to_eval\
+			.replace("x", "*")\
+			.replace("ANS", str(self.previous_result))
+
+		try:
+			if "()" in to_eval or re.match(".*[^+\-*/]\(.*", to_eval):
+				# Checks if there is a non operator before the opening brackets
+				# This avoids it trying to call an int as a function in eval
+				raise SyntaxError
+			result = eval(to_eval)
+		except SyntaxError:
+			result = "SYNTAX ERROR"
+		else:
+			self.previous_result = result
+			result = "=" + str(result)
+		self.add_line(result)
+		self.add_line()
+
+		self.clear_top()
+		self.current_expression = 0
+
+	def clear_top(self):
+		while len(self.lines) > 2 * LINES_TO_SHOW + 1:
+			self.inside_display.removeWidget(self.lines[0])
+			self.lines[0].deleteLater()
+			del self.lines[0]
+
+	def load_expression(self, up=True):
+		if self.current_expression == 0:
+			self.saved_expression = self.lines[-1].text()
+		if up:
+			if len(self.previous_expressions) < abs(self.current_expression - 1):
+				# We have gone as far back as possible
+				return
+			self.current_expression -= 1
+		else:
+			if self.current_expression + 1 > 0:
+				# We have returned gone above value
+				return
+			self.current_expression += 1
+		if self.current_expression == 0:
+			# We have returned to the start
+			expression_to_load = self.saved_expression
+		else:
+			expression_to_load = self.previous_expressions[self.current_expression]
+		self.lines[-1].setText(expression_to_load)
+
+	def keyPressEvent(self, QKeyEvent):
+		text, key = QKeyEvent.text().lower(), QKeyEvent.key()
+		if text in ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "x", "/",
+					"+", "-", "(", ")", ".", "="):
+			self.button_pressed(text)
+		elif text == "*":
+			self.button_pressed("x")
+		elif text == "a":
+			self.button_pressed("ANS")
+		elif key == 16777219:  # DEL
+			self.button_pressed("DEL")
+		elif key == 16777220:  # Enter
+			self.button_pressed("=")
+		elif key == 16777235:  # Up
+			self.load_expression(up=True)
+		elif key == 16777237:  # Down
+			self.load_expression(up=False)
+		elif key == 16777216:  # Escape
+			app.quit()
 
 
 if __name__ == '__main__':
